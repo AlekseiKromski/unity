08c5a197add6e9c48abcb17d248e2670bfc56c4a
alekseiKromski <aleksei.kromski@ivkhk.ee>
2020-11-02 12:43:55 +0200

    create android studio repository

--- UnityLesson/Assets/Scenes/scripts/CanvasButtons.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-public class CanvasButtons : MonoBehaviour
-{
-    public void RestartGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }  
-}
